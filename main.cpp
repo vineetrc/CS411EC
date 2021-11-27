e6ba1ab0dcbcd8632b94e960f3f4b8e42f37e56a
vineetrc <vineetreddi@gmail.com>
2021-11-26 22:52:57 -0600

    bing

--- main.cpp
+++ main.cpp
@@ -14,14 +14,6 @@ using namespace std::chrono;
 // ./a.out ./data/region.tbl REGIONKEY  ./data/nation.tbl HASH  hashtest.tbl
 
 int main(int argc, char *argv[]) {
-    int max = 100;
-
-    std::string str = "vineet";
-    std::hash<std::string> hash_fn;
-
-    double num = std::abs((((int) hash_fn(str))%10000000)/10000000.0);
-
-    std::cout << num << std::endl;
     
     std::string input_file_one = argv[1];
     std::string join_col_one = argv[2];
@@ -61,71 +53,6 @@ int main(int argc, char *argv[]) {
     }
     else
         std::cout << "error, Join Algorithm Not Recognized" << std::endl;
-
-
-
-
-
-
-    // ifstream my_file;
-    // my_file.open("./data/region.tbl");
-    // std::string line;
-    // vector<string> keys{};
-    // int key_idx1 = 0;
-    // int i = 0;
-    // while(getline(my_file, line)){
-    //     //std::cout << line << std::endl;
-        
-    //     vector<string> words{};
-    //     size_t pos = 0;
-    //     std::string delim = "|";
-    //     while ((pos = line.find(delim)) != string::npos) {
-    //         if(i ==0){
-    //             if (line.substr(0, pos).compare("REGIONKEY") == 0){
-    //                 key_idx1 = keys.size();
-    //             }
-    //             keys.push_back(line.substr(0, pos));
-    //         }
-    //         else{
-    //             words.push_back(line.substr(0, pos));
-    //         }
-    //         line.erase(0, pos + delim.length());
-    //     }
-    //     i++;
-    // }
-
-    // for (int i = 0; i < keys.size(); i++){
-    //        std::cout<<keys[i]<<"---";
-    // }
-    
-    // std::cout << key_idx1 << "da one" <<std::endl;
-    
-    
-    
-    
-    //java -cp "*.jar" edu.illinois.netid.Main input1.csv left_col_name1 input2.csv join_col_name2 NESTED_LOOP output.csv
-    //std::cout << "md5 of 'grape': " << md5("grape") << std::endl;
-
-    // long long num1 = 98745636214564698;
-    // long long num2 = 7459874565236544789;
-    // cout << "Product of "<< num1 << " * "<< num2 << " = " << large_product(num1,num2) <<std::endl;
-    //long long number = std::stoi(md5("grape"), 0, 16);
-    // int128_t x;   
-    // std::stringstream ss;
-    // ss << std::hex << "fffefffe";
-    // ss >> x;
-    //std::cout << boost::md5("message").hex_str_value();
-
-    //std::cout << number << std::endl;
-    
-
-    
-
-    
-    // std::cout << argv[1] << std::endl;
-    
-
-
 
     return 0;
 }
