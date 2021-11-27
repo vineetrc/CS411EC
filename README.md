93330b6c040129094b5337e02cb38c476662b8d7
vineetrc <vineetreddi@gmail.com>
2021-11-26 21:06:19 -0600

    added stats box

--- README.md
+++ README.md
@@ -4,21 +4,21 @@
 
 # CS411ExtraCredit
 
-# Commands to run with provided (Nation and Region Files). Run these commands to verify correctness from the direcotry where main.cpp is located
+Commands to run with provided (Nation and Region Files). Run these commands to verify correctness from the direcotry where main.cpp is located
 
 
-Compilation Command: 
+# Compilation Command: 
     g++ -std=c++11  main.cpp join.cpp
 
-Run NESTED LOOP JOIN (write to file looptest.tbl): 
+# Run NESTED LOOP JOIN (write to file looptest.tbl): 
     ./a.out ./data/region.tbl REGIONKEY  ./data/nation.tbl NESTED_LOOP  looptest.tbl
 
-Run HASH JOIN (write to file hashtest.tbl): 
+# Run HASH JOIN (write to file hashtest.tbl): 
     ./a.out ./data/region.tbl REGIONKEY  ./data/nation.tbl HASH  hashtest.tbl
 
-BENCHMARKING
+# BENCHMARKING
 
-# BENCHMARKS Given for the JOIN of LINEITEMS and ORDERS on ORDERKEY
+BENCHMARKS Given for the JOIN of LINEITEMS and ORDERS on ORDERKEY
     ALL MARKS of the AVERAGE of 10 runs:
     
     NESTEDLOOPJOIN (AVG): 144185374 microseconds (around 2.4 minutes)
--- main.cpp
+++ main.cpp
@@ -33,20 +33,31 @@ int main(int argc, char *argv[]) {
     std::cout << "initializing Done" << endl;
 
     if (join_type.compare("HASH") == 0){
-        auto start = high_resolution_clock::now();
-        for (int i = 0; i < 10; i++)
+        int tot = 0;
+        std::cout << "HASH JOIN STATISTICS" << std::endl;
+        for (int i = 0; i < 10; i++){
+            auto start = high_resolution_clock::now();
             algo.HashJoin(output_file);
-        auto stop = high_resolution_clock::now();
-        auto duration = duration_cast<microseconds>(stop - start);
-        cout << duration.count()/10 << "microseconds per join" << endl;
+            auto stop = high_resolution_clock::now();
+            auto duration = duration_cast<microseconds>(stop - start);
+            cout << "Iteration " << i << " took: " << duration.count() << " microseconds" << endl;
+            tot+= duration.count();
+        }
+        cout << "AVG TIME per join is " << tot/10 << " microseconds"<< std::endl;
+            
     }
     else if (join_type.compare("NESTED_LOOP") == 0){
-        auto start = high_resolution_clock::now();
-        for (int i = 0; i < 10; i++)
+        int tot = 0;
+        std::cout << "NESTED LOOP JOIN STATISTICS" << std::endl;
+        for (int i = 0; i < 10; i++){
+            auto start = high_resolution_clock::now();
             algo.LoopJoin(output_file);
-        auto stop = high_resolution_clock::now();
-        auto duration = duration_cast<microseconds>(stop - start);
-        cout << duration.count()/10 << "microseconds per join" << endl;
+            auto stop = high_resolution_clock::now();
+            auto duration = duration_cast<microseconds>(stop - start);
+            cout << "Iteration " << i << " took: " << duration.count() << " microseconds" << endl;
+            tot+= duration.count();
+        }
+        cout << "AVG TIME per join is " << tot/10 << " microseconds"<< std::endl;
     }
     else
         std::cout << "error, Join Algorithm Not Recognized" << std::endl;
